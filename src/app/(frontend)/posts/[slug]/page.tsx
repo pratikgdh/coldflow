5442db89d90ecf120d410bc54072219ce789f079
Pratik Ranjan <134198023+pratikgdh@users.noreply.github.com>
2026-02-25 03:07:53 +0530

    Remove post fetching logic from page.tsx
    Removed fetching and mapping of posts in the page component.

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -15,24 +15,6 @@ import { generateMeta } from '@/utilities/generateMeta'
 import PageClient from './page.client'
 import { LivePreviewListener } from '@/components/LivePreviewListener'
 
-  const posts = await payload.find({
-    collection: 'posts',
-    draft: false,
-    limit: 1000,
-    overrideAccess: false,
-    pagination: false,
-    select: {
-      slug: true,
-    },
-  })
-
-  const params = posts.docs.map(({ slug }) => {
-    return { slug }
-  })
-
-  return params
-}
-
 type Args = {
   params: Promise<{
     slug?: string
